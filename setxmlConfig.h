29a56aebdaa25e245de9c19786e35370d283a23c
Ramnath Sagar <ramnath.sagar@gmail.com>
2014-08-25 22:21:58 -0700

    Moved setxmlConfig from the patch

---
+++ setxmlConfig.h
@@ -0,0 +1,29 @@
+/*
+** $Id: setxmlConfig.h,v 2.005 2013-01-15 23:55:18 rosinski rsai pbellens Exp $
+**
+** Authors: Jim Rosinski
+**          Ramnath Sai
+**          Pieter Bellens
+**
+** XML parser header to parse the xml config file and set the profiling parameters
+*/
+
+
+#ifndef PARSE_H
+#define PARSE_H
+
+#include <stdio.h>
+#include <string.h>
+
+#ifdef HAVE_PAPI
+#include <papi.h>
+#endif
+
+
+
+#include <libxml/parser.h>
+#include "gptl.h"
+#include "private.h"
+
+
+#endif
